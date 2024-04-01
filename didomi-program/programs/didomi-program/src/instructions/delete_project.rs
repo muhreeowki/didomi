1663728f69a03d84d829d3f5af5e45949ff4078c
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-04-01 18:01:53 +0300

    Removed unnecessary variables from delete project instruction

--- didomi-program/programs/didomi-program/src/instructions/delete_project.rs
+++ didomi-program/programs/didomi-program/src/instructions/delete_project.rs
@@ -1,17 +1,15 @@
-use anchor_lang::prelude::*;
-use std::mem::size_of;
-
 use crate::state::ProjectData;
+use anchor_lang::prelude::*;
 
 #[derive(Accounts)]
 pub struct DeleteProject<'info> {
     #[account(mut)]
     pub owner: Signer<'info>,
-    #[account(seeds = [owner.key().as_ref()], bump, close = owner)]
+    #[account(mut, seeds = [owner.key().as_ref()], bump, close = owner)]
     pub project: Account<'info, ProjectData>,
     pub system_program: Program<'info, System>,
 }
 
-pub fn delete_project_handler(ctx: Context<DeleteProject>, data: ProjectData) -> Result<()> {
+pub fn delete_project_handler(_ctx: Context<DeleteProject>) -> Result<()> {
     Ok(())
 }
