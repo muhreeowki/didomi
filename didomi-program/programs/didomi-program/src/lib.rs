8c589087853afd3c16a9e638e05e6d68b43b145c
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-04-01 15:42:54 +0300

    Added create project instruction endpoint in the entrypoint.

--- didomi-program/programs/didomi-program/src/lib.rs
+++ didomi-program/programs/didomi-program/src/lib.rs
@@ -1,5 +1,7 @@
-pub mod state;
 mod instructions;
+pub mod state;
+use self::instructions::*;
+use self::state::*;
 
 use anchor_lang::prelude::*;
 
@@ -8,4 +10,9 @@ declare_id!("4f8xSv3wUdYUe2z343s4M2WRH7Hy2zr7Mdmpi31Y59bY");
 #[program]
 pub mod didomi_program {
     use super::*;
+    // Create project instruction
+    pub fn create_project(ctx: Context<CreateProject>, data: ProjectData) -> Result<()> {
+        instructions::create_project_handler(ctx, data)?;
+        Ok(())
+    }
 }
