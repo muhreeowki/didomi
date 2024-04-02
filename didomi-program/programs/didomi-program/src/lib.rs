a72ece2908c409bda55fd986a89253960cefdd45
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-04-02 19:25:37 +0300

    Added the modified instructions to entrypoint.

--- didomi-program/programs/didomi-program/src/lib.rs
+++ didomi-program/programs/didomi-program/src/lib.rs
@@ -11,14 +11,38 @@ declare_id!("4f8xSv3wUdYUe2z343s4M2WRH7Hy2zr7Mdmpi31Y59bY");
 pub mod didomi_program {
     use super::*;
     // Create project instruction
-    pub fn create_project(ctx: Context<CreateProject>, data: ProjectData) -> Result<()> {
-        instructions::create_project_handler(ctx, data)?;
+    pub fn create_project(
+        ctx: Context<CreateProject>,
+        title: [u8; 64],
+        description: [u8; 256],
+        organizer_name: [u8; 24],
+        target_amount: u64,
+    ) -> Result<()> {
+        instructions::create_project_handler(
+            ctx,
+            title,
+            description,
+            organizer_name,
+            target_amount,
+        )?;
         Ok(())
     }
 
     // Update project instruction
-    pub fn update_project(ctx: Context<UpdateProject>, data: ProjectData) -> Result<()> {
-        instructions::update_project_handler(ctx, data)?;
+    pub fn update_project_handler(
+        ctx: Context<UpdateProject>,
+        title: [u8; 64],
+        description: [u8; 256],
+        organizer_name: [u8; 24],
+        target_amount: u64,
+    ) -> Result<()> {
+        instructions::update_project_handler(
+            ctx,
+            title,
+            description,
+            organizer_name,
+            target_amount,
+        )?;
         Ok(())
     }
 
@@ -28,8 +52,14 @@ pub mod didomi_program {
         Ok(())
     }
 
-    pub fn create_contribution(ctx: Context<CreateContribution>, data: Contribution) -> Result<()> {
-        create_contribution_handler(ctx, data)?;
+    // Contribution instruction
+    pub fn create_contribution(
+        ctx: Context<CreateContribution>,
+        amount: u64,
+        message: [u8; 128],
+        token_type: u8,
+    ) -> Result<()> {
+        instructions::create_contribution_handler(ctx, amount, message, token_type)?;
         Ok(())
     }
 }
