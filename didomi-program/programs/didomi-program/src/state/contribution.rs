266638fcaa77a82a78d85f97f4854ca47562ca0f
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-04-02 18:58:51 +0300

    Modified state accounts
    	- Reduced the project struct becuase it was too large. I reduced the
    	attributes only to those necesarry for computation.
    	- Reduced the message length for the contribution struct.

--- didomi-program/programs/didomi-program/src/state/contribution.rs
+++ didomi-program/programs/didomi-program/src/state/contribution.rs
@@ -9,9 +9,9 @@ pub struct Contribution {
     // amount: The amount of contribution/donation in the smallest unit of the token
     pub amount: u64,
     // message: A fixed-size byte array to store a message or note from the contributor
-    pub message: [u8; 512],
+    pub message: [u8; 128],
     // token_type: A single byte to represent the type of token used for the contribution
     pub token_type: u8,
     // padding: A fixed-size byte array added to ensure proper alignment and padding
-    pub padding: [u8; 32],
+    pub _reserve: [u8; 32],
 }
--- didomi-program/programs/didomi-program/src/state/project.rs
+++ didomi-program/programs/didomi-program/src/state/project.rs
@@ -1,12 +1,11 @@
 use anchor_lang::prelude::*;
-
 // Struct to represent a Project Donation AD
 #[account]
 pub struct ProjectData {
     /// title: Title of the project
     pub title: [u8; 64],
     /// description: Short description about the project
-    pub description: [u8; 512],
+    pub description: [u8; 256],
     /// organizer_name: Name of organizer or onwer of this project
     pub organizer_name: [u8; 24],
     /// organizer_address: Wallet address of the owner of the project.
@@ -16,21 +15,7 @@ pub struct ProjectData {
     /// current_amount: The current amount donated to the project so far
     pub current_amount: u64,
     /// beneficiary_address: Wallet address of the account to recieve donations
-    pub total_contributions: u64,
-    /// beneficiary_address: Wallet address of the account to recieve donations
     pub beneficiary_address: Pubkey,
-    /// start_date: Date project was posted
-    pub start_date: [u8; 24],
-    /// end_date: Deadline to raise funds
-    pub end_date: [u8; 24],
-    /// project_status: Active, Completed, Permanently Open
-    pub project_status: u8,
-    /// project_type: Fundraiser, Trust Fund
-    pub project_type: u8,
-    /// media_urls: List of urls for images and documents
-    pub media_urls: [[u8; 256]; 4],
-    /// project_url: Url for the website of the project
-    pub project_url: [u8; 256],
     /// _reserve: Reserved space
     pub _reserve: [u8; 32],
 }
