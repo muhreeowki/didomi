1819abc6e1db13914d489235541a04e340886930
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-04-05 16:17:23 +0300

    Added User Model to schema and modified models.
    	- Modified models to relate using addresses instead of IDs for easy usage
    	  on the Frontend.
    	- One user model is used to represent project owner and project
    	  contributor.

--- didomi-backend/src/app.module.ts
+++ didomi-backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ProjectsModule } from './projects/projects.module';
+import { ContributionModule } from './contribution/contribution.module';
 
 @Module({
-  imports: [DatabaseModule, ProjectsModule],
+  imports: [DatabaseModule, ProjectsModule, ContributionModule],
   controllers: [AppController],
   providers: [AppService],
 })
