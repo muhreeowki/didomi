77d6a8b9738758fb733aa2f4a5281cc54f8d82f5
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-04-23 06:04:57 +0300

    Updated backend port. Added Cors. Added global exception filter.

--- didomi-backend/src/main.ts
+++ didomi-backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.useGlobalFilters(new HttpExceptionFilter());
+  await app.listen(8000);
 }
 bootstrap();
