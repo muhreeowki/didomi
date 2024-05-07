61033c1894f78e01b164767f24e65a77ac4e96f1
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-05-07 17:05:05 +0300

    Built scaffolding for public project page.
    Used part of a shadcn block for this.

--- didomi-frontend/app/projects/[id]/page.tsx
+++ didomi-frontend/app/projects/[id]/page.tsx
@@ -1,3 +1,68 @@
+import {
+  Bird,
+  Book,
+  Bot,
+  Code2,
+  CornerDownLeft,
+  LifeBuoy,
+  Mic,
+  Paperclip,
+  Rabbit,
+  Settings,
+  Settings2,
+  Share,
+  SquareTerminal,
+  SquareUser,
+  Triangle,
+  Turtle,
+} from "lucide-react";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
 import React from "react";
 import axios from "axios";
 
@@ -10,11 +75,171 @@ const Project = async ({ params }: any) => {
     .catch(console.error);
 
   return (
-    <section className="grid gap-4 grid-cols-12">
-      <img className="col-span-5 w-full" src={project.imageURL} />
-      <div></div>
-    </section>
+    <TooltipProvider>
+      <div className="grid h-full w-full">
+        <div className="flex flex-col">
+          <header className="sticky top-0 z-10 flex h-[53px] items-center gap-1 border-b bg-background px-4">
+            <h1 className="text-4xl font-semibold">{project.title}</h1>
+            <AlertDialog>
+              <AlertDialogTrigger className="ml-auto ">
+                <Button variant="outline" size="sm" className="gap-1.5 text-sm">
+                  <Share className="size-3.5" />
+                  Share
+                </Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    This action cannot be undone. This will permanently delete
+                    your account and remove your data from our servers.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogAction>Done</AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </header>
+          <main className="grid flex-1 gap-4 overflow-auto p-4 md:grid-cols-2 lg:grid-cols-4">
+            <div className="flex flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
+              <img
+                src={project.imageURL}
+                className="rounded-xl aspect-square"
+              />
+            </div>
+            <div
+              className="flex flex-row md:flex-col items-start gap-8 col-span-2"
+              x-chunk="dashboard-03-chunk-0"
+            >
+              <div className="grid w-full items-start gap-6">
+                <div className="grid gap-6 rounded-lg border p-4">
+                  <legend className="-ml-1 px-1 text-sm font-medium">
+                    Settings
+                  </legend>
+                  <div className="grid gap-3">
+                    <Label htmlFor="model">Model</Label>
+                    <Select>
+                      <SelectTrigger
+                        id="model"
+                        className="items-start [&_[data-description]]:hidden"
+                      >
+                        <SelectValue placeholder="Select a model" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="genesis">
+                          <div className="flex items-start gap-3 text-muted-foreground">
+                            <Rabbit className="size-5" />
+                            <div className="grid gap-0.5">
+                              <p>
+                                Neural{" "}
+                                <span className="font-medium text-foreground">
+                                  Genesis
+                                </span>
+                              </p>
+                              <p className="text-xs" data-description>
+                                Our fastest model for general use cases.
+                              </p>
+                            </div>
+                          </div>
+                        </SelectItem>
+                        <SelectItem value="explorer">
+                          <div className="flex items-start gap-3 text-muted-foreground">
+                            <Bird className="size-5" />
+                            <div className="grid gap-0.5">
+                              <p>
+                                Neural{" "}
+                                <span className="font-medium text-foreground">
+                                  Explorer
+                                </span>
+                              </p>
+                              <p className="text-xs" data-description>
+                                Performance and speed for efficiency.
+                              </p>
+                            </div>
+                          </div>
+                        </SelectItem>
+                        <SelectItem value="quantum">
+                          <div className="flex items-start gap-3 text-muted-foreground">
+                            <Turtle className="size-5" />
+                            <div className="grid gap-0.5">
+                              <p>
+                                Neural{" "}
+                                <span className="font-medium text-foreground">
+                                  Quantum
+                                </span>
+                              </p>
+                              <p className="text-xs" data-description>
+                                The most powerful model for complex
+                                computations.
+                              </p>
+                            </div>
+                          </div>
+                        </SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                  <div className="grid gap-3">
+                    <Label htmlFor="temperature">Temperature</Label>
+                    <Input id="temperature" type="number" placeholder="0.4" />
+                  </div>
+                  <div className="grid grid-cols-2 gap-4">
+                    <div className="grid gap-3">
+                      <Label htmlFor="top-p">Top P</Label>
+                      <Input id="top-p" type="number" placeholder="0.7" />
+                    </div>
+                    <div className="grid gap-3">
+                      <Label htmlFor="top-k">Top K</Label>
+                      <Input id="top-k" type="number" placeholder="0.0" />
+                    </div>
+                  </div>
+                </div>
+                <div className="grid gap-6 rounded-lg border p-4">
+                  <legend className="-ml-1 px-1 text-sm font-medium">
+                    Messages
+                  </legend>
+                  <div className="grid gap-3">
+                    <Label htmlFor="role">Role</Label>
+                    <Select defaultValue="system">
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a role" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="system">System</SelectItem>
+                        <SelectItem value="user">User</SelectItem>
+                        <SelectItem value="assistant">Assistant</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                  <div className="grid gap-3">
+                    <Label htmlFor="content">Content</Label>
+                    <Textarea
+                      id="content"
+                      placeholder="You are a..."
+                      className="min-h-[9.5rem]"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="w-full col-span-full bg-background">
+              <Card>
+                <CardHeader>
+                  <CardTitle>About</CardTitle>
+                  <CardDescription>Card Description</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p>{project.story}</p>
+                </CardContent>
+                <CardFooter>
+                  <a href={project.websiteURL}>Click here to see more</a>
+                </CardFooter>
+              </Card>
+            </div>
+          </main>
+        </div>
+      </div>
+    </TooltipProvider>
   );
 };
-
 export default Project;
