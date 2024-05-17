4655686c48309a5c7debbf910d3402e5875c936a
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-05-17 15:11:35 +0300

    Final touches to landing page.
    - Using static gradient background instead of animation. This is due to
      cpu usage during testing.

--- didomi-frontend/app/page.tsx
+++ didomi-frontend/app/page.tsx
@@ -10,20 +10,30 @@ import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
 import { useRouter } from "next/navigation";
 import { useDidomiContext } from "@/context";
 import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
+import Navbar from "@/components/Navbar";
 
 const Home = () => {
   const router = useRouter();
   const { data: session } = useSession();
 
   return (
-    <main className="absolute top-0 left-0 h-screen w-screen">
-      <BackgroundGradientAnimation>
-        <div className="absolute z-30 inset-0 flex flex-col items-center justify-center lg:justify-end mb-[80px] text-white font-bold px-4 text-3xl  md:text-4xl lg:text-6xl text-center">
+    <>
+      <Navbar />
+      <main
+        className="absolute top-0 left-0 h-screen w-screen"
+        style={{
+          backgroundColor: "hsla(0,100%,50%,1)",
+          backgroundImage:
+            "radial-gradient(at 40% 20%, hsla(28,100%,74%,1) 0px, transparent 50%), radial-gradient(at 80% 0%, hsla(189,100%,56%,1) 0px, transparent 50%), radial-gradient(at 0% 50%, hsla(355,100%,93%,1) 0px, transparent 50%), radial-gradient(at 80% 50%, hsla(340,100%,76%,1) 0px, transparent 50%), radial-gradient(at 0% 100%, hsla(22,100%,77%,1) 0px, transparent 50%), radial-gradient(at 80% 100%, hsla(242,100%,70%,1) 0px, transparent 50%), radial-gradient(at 0% 0%, hsla(343,100%,76%,1) 0px, transparent 50%)",
+        }}
+      >
+        {/*<BackgroundGradientAnimation>*/}
+        <div className="absolute z-30 inset-0 flex flex-col items-center justify-center text-white font-bold px-4 text-3xl  md:text-4xl lg:text-5xl text-center">
           <span className="pointer-events-none">
-            <h1 className="scroll-m-20 text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20 text-6xl md:text-8xl font-semibold tracking-wider lg:text-[148px] bg-clip-text">
-              DIDOMI FUND
+            <h1 className="scroll-m-20 text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/40 text-6xl md:text-8xl font-semibold tracking-wider lg:text-[148px] bg-clip-text">
+              Didomi Fund
             </h1>
-            <p className="mt-6 bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
+            <p className="mt-6 bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/40">
               The future of Crypto Crowdfunding and Charity
             </p>
           </span>
@@ -37,8 +47,9 @@ const Home = () => {
             </Button>
           </div>
         </div>
-      </BackgroundGradientAnimation>
-    </main>
+        {/*</BackgroundGradientAnimation>*/}
+      </main>
+    </>
   );
 };
 
